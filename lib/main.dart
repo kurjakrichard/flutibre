df0f4c7aef7223ecbbbd0164f0010b356098d0bb
kurjakrichard <kurjak.richard@gmail.com>
2021-09-22 19:22:20 +0200

    picture and button

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(fontFamily: 'Raleway'),
     debugShowCheckedModeBanner: false,
     title: "Flutibre",
     home: Home(),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,17 +9,92 @@ class Home extends StatelessWidget {
       alignment: Alignment.center,
       color: Colors.deepPurple,
       margin: const EdgeInsets.all(10.0),
-      child: const Text(
-        'Flight',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            decoration: TextDecoration.none,
-            fontSize: 75.0,
-            fontFamily: 'Raleway',
-            fontWeight: FontWeight.w700,
-            fontStyle: FontStyle.italic,
-            color: Colors.deepOrange),
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Column(
+            children: const [
+              Text(
+                'Első',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontFamily: 'Raleway',
+                    fontSize: 55.0,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.deepOrange),
+              ),
+              Expanded(
+                  child: Text(
+                'Második',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontFamily: 'Roboto Slab',
+                    fontSize: 55.0,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.deepOrange),
+              )),
+              Expanded(
+                  child: Text(
+                'Ez egy hosszabb szöveg tesztelésre',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontFamily: 'Roboto Slab',
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.deepOrange),
+              )),
+            ],
+          ),
+          const HommImageAsset(),
+          const HommButton()
+        ],
       ),
     ));
+  }
+}
+
+class HommImageAsset extends StatelessWidget {
+  const HommImageAsset({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = const AssetImage('images/48884.png');
+    Image image = Image(image: assetImage, width: 50, height: 50);
+    return Container(child: image);
+  }
+}
+
+class HommButton extends StatelessWidget {
+  const HommButton({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 110,
+        height: 50,
+        child: ElevatedButton(
+            onPressed: () {
+              alertDialog(context);
+            },
+            child: const Text(
+              'Enabled',
+              style: TextStyle(fontSize: 20, color: Colors.cyanAccent),
+            )));
+  }
+
+  void alertDialog(BuildContext context) {
+    var alertDialog = const AlertDialog(
+      title: Text('Siker'),
+      content: Text('Sikerült'),
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
   }
 }
