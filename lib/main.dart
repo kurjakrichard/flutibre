b1e899001352248a1dd3b2d31954c29611e9e48f
kurjakrichard <kurjak.richard@gmail.com>
2022-02-22 02:09:22 +0100

    Refactor provider

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,17 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(const Flutibre());
+void main() {
+  Provider.debugCheckInvalidValueType = null;
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BookProvider()),
+      ],
+      child: const Flutibre(),
+    ),
+  );
+}
 
 class Flutibre extends StatelessWidget {
   const Flutibre({Key? key}) : super(key: key);
@@ -19,82 +29,67 @@ class Flutibre extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<BookProvider>(
-      create: (context) => BookProvider(),
-      child: MaterialApp(
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale('en', ''),
-          Locale('hu', ''),
-        ],
+    return MaterialApp(
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('hu', ''),
+      ],
 
-        locale: const Locale('hu'),
-        debugShowCheckedModeBanner: false,
-        //Scrolling do not work on Linux desktop without this.
-        scrollBehavior: MyCustomScrollBehavior(),
-        home: const ResponsiveHomePage(),
-        routes: {
-          '/ListPage': (context) => const ListPage(),
-          '/GridPage': (context) => const GridPage(),
-          '/AddBookPage': (context) =>
-              ModifyBookPage(title: AppLocalizations.of(context)!.addbook),
-          '/BookDetailsPage': (context) => const BookDetailsPage(),
-        },
+      locale: const Locale('hu'),
+      debugShowCheckedModeBanner: false,
+      //Scrolling do not work on Linux desktop without this.
+      scrollBehavior: MyCustomScrollBehavior(),
+      home: const ResponsiveHomePage(),
+      routes: {
+        '/ListPage': (context) => const ListPage(),
+        '/GridPage': (context) => const GridPage(),
+        '/AddBookPage': (context) =>
+            ModifyBookPage(title: AppLocalizations.of(context)!.addbook),
+        '/BookDetailsPage': (context) => const BookDetailsPage(),
+      },
 
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.cyan,
-            foregroundColor: Colors.white,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.cyan,
+          foregroundColor: Colors.white,
+        ),
+        scaffoldBackgroundColor: Colors.cyan[50],
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: Colors.cyan, foregroundColor: Colors.white),
+        fontFamily: 'Roboto',
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            onPrimary: Colors.white,
+            primary: Colors.cyan,
           ),
-          scaffoldBackgroundColor: Colors.cyan[50],
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-              backgroundColor: Colors.cyan, foregroundColor: Colors.white),
-          fontFamily: 'Roboto',
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              onPrimary: Colors.white,
-              primary: Colors.cyan,
-            ),
-          ),
-          textTheme: const TextTheme(
-            //fejléc szövegek
-            headline1: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: Colors.white,
-                fontSize: 18),
-            //belső fejléc szövegek
-            subtitle1: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-                fontSize: 16),
-            subtitle2: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-                fontSize: 15),
-            //gombszövegek
-            headline3: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: Colors.white,
-                fontSize: 15),
-            //nagyobb belső szövegek
-            bodyText1: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-                fontSize: 15),
-            //kisebb belső szövegek
-            bodyText2: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-                fontSize: 12),
-            //vastag belső szövegek
-            headline2: TextStyle(
-                fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15),
-          ),
+        ),
+        textTheme: const TextTheme(
+          //fejléc szövegek
+          headline1: TextStyle(
+              fontWeight: FontWeight.normal, color: Colors.white, fontSize: 18),
+          //belső fejléc szövegek
+          subtitle1: TextStyle(
+              fontWeight: FontWeight.normal, color: Colors.black, fontSize: 16),
+          subtitle2: TextStyle(
+              fontWeight: FontWeight.normal, color: Colors.black, fontSize: 15),
+          //gombszövegek
+          headline3: TextStyle(
+              fontWeight: FontWeight.normal, color: Colors.white, fontSize: 15),
+          //nagyobb belső szövegek
+          bodyText1: TextStyle(
+              fontWeight: FontWeight.normal, color: Colors.black, fontSize: 15),
+          //kisebb belső szövegek
+          bodyText2: TextStyle(
+              fontWeight: FontWeight.normal, color: Colors.black, fontSize: 12),
+          //vastag belső szövegek
+          headline2: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 15),
         ),
       ),
     );
